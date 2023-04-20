acb639fd8479dba739756b143ba66392f756e1da
reidmedlock <benmedlock19@gmail.com>
2023-04-19 20:27:11 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+static int calculateArea(int lengthInFeet, int widthInFeet)
+{
+    return lengthInFeet * widthInFeet;
+}
+
+{
+    int lengthInFeet = 0;
+    int widthInFeet = 0;
+    int classroom = 0;
+
+
+    Console.Write("Enter length of the Classroom in feet: ");
+    lengthInFeet = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter width of Classroom in feet: ");
+    widthInFeet = Convert.ToInt32(Console.ReadLine());
+
+    classroom = calculateArea(lengthInFeet, widthInFeet);
+
+    Console.WriteLine("The classroom  is " + classroom + " square feet.");
+
+    if (classroom < 250)
+    {
+        Console.WriteLine("The class is small.");
+    }
+
+    if (classroom > 650)
+    {
+        Console.WriteLine("The class is large.");
+    }
+
+    else if (classroom < 650 && classroom > 250)
+    {
+        Console.WriteLine("The class is medium.");
+    }
+}
+Console.WriteLine("Thank you for using the Class Size Calculator!");
+
+
+
+
